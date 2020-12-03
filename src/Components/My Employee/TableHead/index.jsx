b0d8ba3cc8cfb18feafdf922e27320ba2903f5c7
Ahmed Kortas <42817639+ahmedkortas@users.noreply.github.com>
2020-12-03 21:31:07 +0100

    fixed bugs and errors

--- src/Components/My Employee/TableHead/index.jsx
+++ src/Components/My Employee/TableHead/index.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./style.css";
 
-
 function TableHead(props) {
-    return (
-        <thead className="thead">
-            <tr>
-                {props.children}
-            </tr>
-        </thead>
-    )
+  return (
+    <thead className="thead">
+      <th>name</th>
+      <th>email</th>
+      <th>Phone Number</th>
+    </thead>
+  );
 }
 
 export default TableHead;
--- src/Components/My Employee/TableRow/index.jsx
+++ src/Components/My Employee/TableRow/index.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import "./style.css";
 
-
-
-
 function TableRow(props) {
-    return (
-        <tr>
-            <th scope="row">{props.id}</th>
-            <td style={{width:'200px'}}>{props.first_name}</td>
-            <td>{props.last_name}</td>
-            <td>{props.department}</td>
-            <td>{props.role}</td>
-            <td>{props.email}</td>
-        </tr>
-    )
+  return (
+    <tr>
+      <th scope="row">{props.id}</th>
+      <td style={{ width: "200px" }}>{props.first_name}</td>
+      <td>{props.name}</td>
+      <td>{props.phoneNumber}</td>
+      <td>{props.email}</td>
+    </tr>
+  );
 }
 
 export default TableRow;
